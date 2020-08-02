1ad3fb70031ebf2d8b10c858cf22df99ff3215b5
akshay-col <67306639+akshay-col@users.noreply.github.com>
2020-08-02 16:34:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,15 +2,15 @@ class Translation(object):
     START_TEXT = """ <b>I'm A Simple File Renamer Bot With Permanent Thumbnail support!💯</b>
 
 <b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>
-<b>Bot Maintained By: @Mai_bOTs</b>"""
+<b>Bot Maintained By: @noob_admins</b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
-    DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
+    UPGRADE_TEXT = "CONTACT @Noob_admin "
+    DOWNLOAD_START = "<b>💢Downloading To My server !! pls Wait💢</b>"
     UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me JOin > @Mai_bOTs  ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me   ❤️**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @No_OnE_Kn0wS_Me "
