d89f974aabe82901f6676dfc72cdbb8acc8ca7ed
akshay-col <67306639+akshay-col@users.noreply.github.com>
2020-08-06 12:12:34 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,25 +2,25 @@ class Translation(object):
     START_TEXT = """ <b>I'm A Simple File Renamer Bot With Permanent Thumbnail support!💯</b>
 
 <b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>
-<b>Bot Maintained By: @noob_admins</b>"""
+<b>Bot Maintained By: @noob_admin</b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @Noob_admin "
     DOWNLOAD_START = "<b>💢Downloading To My server !! pls Wait💢</b>"
-    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    UPLOAD_START = "<b>♨️Downloading Completed Now I'm Uploading Into TG ♨️</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me   ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me use after 2 minutes 🥴**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @Noob_admin "
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @No_OnE_Kn0wS_Me "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @Noob_admin "
     SAVED_CUSTOM_THUMB_NAIL = "<b>Custom thumbnail Saved ✅ This Is Permanent Until</b> /deletethumbnail❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "<b>♨️File Downloaded Successfully ♨️</b>"
     REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "THUMBNAIL ONNUM ELLA,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
     HELP_USER = """Ohh You Want Help?? 😅
