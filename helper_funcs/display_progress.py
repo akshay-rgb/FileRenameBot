6dec91b298bde5d7b64be19fba9b7c57ddae6664
akshay-col <67306639+akshay-col@users.noreply.github.com>
2020-08-09 19:02:23 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -47,7 +47,7 @@ async def progress_for_pyrogram(
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>✅Completed:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>🚀Speed:</b> {2}/s\n<b>⏳ETA:</b> {3}\n".format(
+        tmp = progress + "<b>✅DONE:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>⚡Speed:</b> {2}/s\n<b>⏳ETA:</b> {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
