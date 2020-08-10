420385faf1252b9f2fdb2461bb0152c52ad51bd9
꧁☬⋆ТᎻᎬ༒ᏦᎥᏁᏳ༒C☢nτrͥ☢lͣlͫer⋆☬꧂ <67306639+noob-admin@users.noreply.github.com>
2020-08-10 06:53:17 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -54,25 +54,13 @@ async def start(bot, update):
     )
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
 async def upgrade(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    TRChatBase(update.from_user.id, update.text, "/about")
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.UPGRADE_TEXT,
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
-    )
-     @pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/about")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.ABOUT_USER,format(chat_id,plan_type,expires_at),
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
